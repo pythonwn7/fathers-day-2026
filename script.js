156d7afeb11635794a82a3b081d8c5db14dc0679
zakham-111 <zakir.hamza0905@gmail.com>
2026-06-21 02:11:55 -0500

    Create Father's Day reveal page

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function goToSurprise() {
     window.location.href = "surprise.html";
 }
+window.onload = function() {
+
+    const button = document.getElementById("second-button");
+
+
+    if (button) {
+
+        setTimeout(function() {
+
+            button.classList.remove("hidden-button");
+
+            button.classList.add("show-button");
+
+        }, 4000);
+
+    }
+
+};
