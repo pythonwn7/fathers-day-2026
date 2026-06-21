a347fc8455d22cc78bdc44bf7b6d24df1153b042
zakham-111 <zakir.hamza0905@gmail.com>
2026-06-21 02:17:15 -0500

    Confetti addition

--- script.js
+++ script.js
@@ -19,3 +19,49 @@ window.onload = function() {
     }
 
 };
+
+function createConfetti() {
+
+    const container = document.getElementById("confetti-container");
+
+    if (!container) {
+        return;
+    }
+
+
+    for (let i = 0; i < 100; i++) {
+
+        let piece = document.createElement("div");
+
+        piece.className = "confetti-piece";
+
+
+        piece.style.left = Math.random() * 100 + "vw";
+
+
+        piece.style.animationDelay = Math.random() * 2 + "s";
+
+
+        piece.style.backgroundColor =
+            "hsl(" + Math.random() * 360 + ", 100%, 50%)";
+
+
+        container.appendChild(piece);
+
+
+        setTimeout(function() {
+
+            piece.remove();
+
+        }, 5000);
+
+    }
+
+}
+
+
+window.addEventListener("load", function() {
+
+    createConfetti();
+
+});
